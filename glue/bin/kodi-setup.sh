e07c8a66b80c0434d67038d53da1ae6a1b864c2d
Alan Griffiths <alan@octopull.co.uk>
2019-07-30 17:56:05 +0100

    Better setup

--- glue/bin/kodi-setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-for PLUG in alsa avahi-observe hardware-observe locale-control mount-observe network-observe removable-media shutdown system-observe; do sudo snap connect egmde-confined-desktop:${PLUG}; done
---
+++ glue/bin/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# For kodi
+for PLUG in alsa avahi-observe hardware-observe locale-control mount-observe network-observe removable-media shutdown system-observe; do sudo snap connect egmde-confined-desktop:${PLUG}; done
+
+# For DM
+sudo snap connect egmde-confined-desktop:login-session-control
+if [ -d "/usr/share/wayland-sessions/" ]
+then sudo cp /snap/egmde-confined-desktop/current/usr/share/wayland-sessions/egmde-confined-desktop.desktop /usr/share/wayland-sessions/
+fi
