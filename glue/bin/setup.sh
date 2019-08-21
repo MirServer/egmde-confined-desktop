2578b166a4ab726299961a98e0035c997ed7d815
Alan Griffiths <alan@octopull.co.uk>
2019-08-21 09:22:28 +0100

    Cut down "other apps" and set up egmde to launch apps using snapcraft.Launch.

--- glue/bin/setup.sh
+++ glue/bin/setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# For kodi
-for PLUG in alsa avahi-observe hardware-observe locale-control mount-observe network-observe removable-media shutdown system-observe; do sudo snap connect egmde-confined-desktop:${PLUG}; done
-
 # For DM
 sudo snap connect egmde-confined-desktop:login-session-control
 if [ -d "/usr/share/wayland-sessions/" ]
